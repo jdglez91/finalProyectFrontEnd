5ca392198fecc8011986a51069bd641340154032
jdglez91 <jdglez1991@gmail.com>
2020-11-10 01:40:29 +0000

    incomplete

--- src/js/layout.js
+++ src/js/layout.js
@@ -17,7 +17,7 @@ const Layout = () => {
 	const basename = process.env.BASENAME || "";
 
 	return (
-		<div className="d-flex flex-column h-100">
+		<div className="container" style={{ background: "#bbe6da" }}>
 			<BrowserRouter basename={basename}>
 				<ScrollToTop>
 					<Navbar />
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -9,23 +9,14 @@ export const Home = () => (
 				<img src="https://img.icons8.com/fluent-systems-regular/144/000000/housekeeping.png" />
 			</p>
 			<h2>Log in</h2>
-			<form>
+			<form style={{ width: "400px", margin: "auto" }}>
 				<div className="form-group">
 					<label For="exampleInputEmail1">Email address</label>
 					<input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-					<small id="emailHelp" className="form-text text-muted">
-						{"We'll never share your email with anyone else."}
-					</small>
 				</div>
 				<div className="form-group">
 					<label For="exampleInputPassword1">Password</label>
 					<input type="password" className="form-control" id="exampleInputPassword1" />
-				</div>
-				<div className="form-group form-check">
-					<input type="checkbox" className="form-check-input" id="exampleCheck1" />
-					<label className="form-check-label" For="exampleCheck1">
-						Check me out
-					</label>
 				</div>
 				<button type="submit" className="btn btn-primary">
 					Submit
