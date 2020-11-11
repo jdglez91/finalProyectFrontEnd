f970d0576175ce507697d33909f9df06ff487848
jdglez91 <jdglez1991@gmail.com>
2020-11-11 23:26:54 +0000

    incomplete

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav>
-			<Link to="/">
+		<nav className="container">
+			{/* <Link to="/">
 				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
+			</Link> */}
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<button className="btn btn-primary">Sign up</button>
 				</Link>
 			</div>
 		</nav>
