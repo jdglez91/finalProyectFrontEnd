f66cdf4e77262e5d32b53dfd814dcf07a98fed0d
jdglez91 <jdglez1991@gmail.com>
2020-11-12 02:06:56 +0000

    incomplete

--- src/js/views/main.js
+++ src/js/views/main.js
@@ -1,4 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export const Main = () => <div className="container" />;
+export const Main = () => {
+	return (
+		<div className="container">
+			<div>
+				<div>
+					<img src="https://img.icons8.com/windows/200/000000/broom.png" />
+				</div>
+				<div>
+					<img src="https://img.icons8.com/pastel-glyph/200/000000/vacuum-cleaner--v3.png" />
+				</div>
+				<div>
+					<img src="https://img.icons8.com/fluent-systems-regular/200/000000/service-bell.png" />
+				</div>
+			</div>
+		</div>
+	);
+};
