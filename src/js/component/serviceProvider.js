4bbedc2bb4e15a1740e1691f7010f970cd51fb6f
jdglez91 <jdglez1991@gmail.com>
2020-11-17 02:41:08 +0000

    incomplete

--- src/js/component/serviceProvider.js
+++ src/js/component/serviceProvider.js
@@ -2,23 +2,26 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import PropTypes from "prop-types";
 
-export const Search = props => {
+export const ServiceCard = props => {
 	const { actions, store } = useContext(Context);
 	return (
-		<div ClassName="container-fluid">
-			<div className="card mb-3" style="max-width: 540px;">
-				<div className="row no-gutters">
-					<div className="col-md-4">
-						<img src="..." className="card-img" alt="..." />
-					</div>
-					<div className="col-md-8">
-						<div className="card-body">
-							<h5 className="card-title">{props.provider.name}</h5>
-							<p className="card-text">Provides the following services: {props.provider.type}</p>
-							<p className="card-text">
-								<small className="text-muted">{props.provider.hourlyRate}</small>
-							</p>
-						</div>
+		<div
+			className="card mb-3"
+			style={{ maxWidth: "540px" }}
+			onClick={() => {
+				props.setSelect(!props.select);
+			}}>
+			<div className="row no-gutters">
+				<div className="col-md-4">
+					<img src="..." className="card-img" alt="..." />
+				</div>
+				<div className="col-md-8">
+					<div className="card-body">
+						<h5 className="card-title">{props.provider.name}</h5>
+						<p className="card-text">Provides the following services: {props.provider.type}</p>
+						<p className="card-text">
+							<small className="text-muted">{props.provider.hourlyRate}</small>
+						</p>
 					</div>
 				</div>
 			</div>
@@ -26,7 +29,9 @@ export const Search = props => {
 	);
 };
 
-Search.propTypes = {
+ServiceCard.propTypes = {
 	provider: PropTypes.object,
-	index: PropTypes.number
+	index: PropTypes.number,
+	setSelect: PropTypes.func,
+	select: PropTypes.bool
 };
--- src/js/views/serviceRequest.js
+++ src/js/views/serviceRequest.js
@@ -1,25 +1,45 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 import { Search } from "../component/search";
+import { ServiceCard } from "../component/serviceProvider";
 
 export const ServiceRequest = () => {
+	const { store } = useContext(Context);
+	const [select, setSelect] = useState(false);
+	let mapServices = store.servicesProviders.map(service => {
+		return (
+			<ServiceCard
+				className="col-sm"
+				key={service.name}
+				provider={service}
+				setSelect={setSelect}
+				select={select}
+			/>
+		);
+	});
 	return (
 		<div className="container">
 			<div className="row">
-				<div className="col-sm">Calendar</div>
-				<div className="col-sm">
-					<div className="form-group">
-						<label For="exampleFormControlTextarea1">Description</label>
-						<textarea className="form-control" id="exampleFormControlTextarea1" rows="3" />
-					</div>
-				</div>
-				<div className="col-sm">
-					<Link to="/main">
-						<button type="submit" className="btn btn-primary">
-							Submit
-						</button>
-					</Link>
-				</div>
+				{!select ? (
+					<>{mapServices}</>
+				) : (
+					<>
+						<div className="col-sm">
+							<div className="form-group">
+								<label For="exampleFormControlTextarea1">Description</label>
+								<textarea className="form-control" id="exampleFormControlTextarea1" rows="3" />
+							</div>
+						</div>
+						<div className="col-sm">
+							<Link to="/main">
+								<button type="submit" className="btn btn-primary">
+									Submit
+								</button>
+							</Link>
+						</div>
+					</>
+				)}
 			</div>
 			<Search />
 		</div>
